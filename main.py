cff1ccd82d0a82ab92e6507b782eb4f4a55c97d4
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-03-09 00:23:45 +0100

    Connect with db

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("mleko")
+import psycopg2
+
+con = psycopg2.connect(database="postgres", user="postgres", password="password", host="127.0.0.1", port="5432")
+
+print("Database opened successfully")
