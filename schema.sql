e0232dd04f8b5390eb615626a6a663434658dbdf
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-04-27 23:27:14 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,26 @@
+-- public."user" definition
+
+-- Drop table
+
+-- DROP TABLE public."user";
+
 CREATE TABLE public."user" (
 	id int8 NOT NULL,
 	"name" varchar NULL,
 	"location" varchar NULL,
 	description varchar NULL,
 	followers_count int4 NULL,
+	friends_count int4 NULL,
+	media_count int4 NULL,
+	favourites_count int4 NULL,
 	CONSTRAINT user_pkey PRIMARY KEY (id)
 );
+
+-- public.tweet definition
+
+-- Drop table
+
+-- DROP TABLE public.tweet;
 
 CREATE TABLE public.tweet (
 	id int8 NOT NULL,
@@ -23,10 +38,35 @@ CREATE TABLE public.tweet (
 	quote_count int4 NULL,
 	reply_to int8 NULL,
 	fetched_comments bool NULL,
+	"type" int4 NULL,
+	fetched_retweets bool NULL,
+	fetched_quotes bool NULL,
 	CONSTRAINT tweet_pk PRIMARY KEY (id)
 );
 
-ALTER TABLE public.tweet ADD CONSTRAINT fk_tweet_user null;
+
+-- public.tweet foreign keys
+
+ALTER TABLE public.tweet ADD CONSTRAINT fk_tweet_user FOREIGN KEY (user_id) REFERENCES "user"(id);
+
+-- public.retweet definition
+
+-- Drop table
+
+-- DROP TABLE public.retweet;
+
+CREATE TABLE public.retweet (
+	id bigserial NOT NULL,
+	user_id int8 NULL,
+	tweet_id int8 NULL,
+	CONSTRAINT retweet_pk PRIMARY KEY (id)
+);
+
+
+-- public.retweet foreign keys
+
+ALTER TABLE public.retweet ADD CONSTRAINT retweet_fk FOREIGN KEY (user_id) REFERENCES "user"(id);
+ALTER TABLE public.retweet ADD CONSTRAINT retweet_fk_1 FOREIGN KEY (tweet_id) REFERENCES tweet(id);
 
 CREATE TABLE public."cursor" (
 	id serial NOT NULL,
@@ -36,9 +76,11 @@ CREATE TABLE public."cursor" (
 	CONSTRAINT cursor_pk PRIMARY KEY (id)
 );
 
-CREATE SEQUENCE cursor_id_seq;
-
-ALTER SEQUENCE cursor_id_seq OWNED BY "cursor".id;
+--CREATE SEQUENCE cursor_id_seq;
+--ALTER SEQUENCE cursor_id_seq OWNED BY "cursor".id;
+--
+--CREATE SEQUENCE retweet_id_seq;
+--ALTER SEQUENCE retweet_id_seq OWNED BY retweet.id;
 
 INSERT INTO public."cursor"
 (id, "date", "cursor", count)
