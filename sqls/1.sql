25af9c8b01732a0ae4b09d4bb65ada96e98179b8
jakubowiczish <jakub77plotnikowski@gmail.com>
2021-05-13 20:09:08 +0200

    Add jupyter notebook for 1.sql

--- sqls/1.sql
+++ sqls/1.sql
@@ -1,5 +1,5 @@
 -- top 10 userów którzy napisali najwięcej tweetów
-select u.name, count(*) as "Liczba tweetów"
+select u.name as "Nazwa użytkownika", count(*) as "Liczba tweetów"
 from "user" u
          join tweet t on u.id = t.user_id
 where t.reply_to is null
@@ -9,7 +9,7 @@ order by count(*) desc
 limit 10;
 
 -- top 10 userów którzy napisali najwięcej komentarzy
-select u.name, count(*) as "Liczba komentarzy"
+select u.name as "Nazwa użytkownika", count(*) as "Liczba komentarzy"
 from "user" u
          join tweet t on u.id = t.user_id
 where t.type = 2
@@ -18,7 +18,7 @@ order by count(*) desc
 limit 10;
 
 -- top 10 userów którzy napisali najwięcej pozytywnych tweetow
-select u.name, count(*) as "Liczba pozytywnych tweetow"
+select u.name as "Nazwa użytkownika", count(*) as "Liczba pozytywnych tweetow"
 from "user" u
          join tweet t on u.id = t.user_id
 where t.reply_to is null
@@ -29,7 +29,7 @@ order by count(*) desc
 limit 10;
 
 -- top 10 userów którzy napisali najwięcej negatywnych tweetow
-select u.name, count(*) as "Liczba negatywnych tweetow"
+select u.name as "Nazwa użytkownika", count(*) as "Liczba negatywnych tweetow"
 from "user" u
          join tweet t on u.id = t.user_id
 where t.reply_to is null
@@ -41,7 +41,7 @@ limit 10;
 
 
 -- top 10 userów którzy napisali najwięcej pozytywnych komentarzy
-select u.name, count(*) as "Liczba pozytywnych komentarzy"
+select u.name as "Nazwa użytkownika", count(*) as "Liczba pozytywnych komentarzy"
 from "user" u
          join tweet t on u.id = t.user_id
 where t.type = 2
@@ -51,7 +51,7 @@ order by count(*) desc
 limit 10;
 
 -- top 10 userów którzy napisali najwięcej negatywnych komentarzy
-select u.name, count(*) as "Liczba negatywnych komentarzy"
+select u.name as "Nazwa użytkownika", count(*) as "Liczba negatywnych komentarzy"
 from "user" u
          join tweet t on u.id = t.user_id
 where t.type = 2
@@ -61,8 +61,8 @@ order by count(*) desc
 limit 10;
 
 -- top 10 najpopularniejszych tweetow z największą ilością komentarzy + retweetów + cytowań + polubień
-select t.id, t.content, t.reply_count + t.retweet_count + t.quote_count + t.favorite_count as sum
+select t.id as "Identyfikator tweeta", t.content, t.reply_count + t.retweet_count + t.quote_count + t.favorite_count as "Suma komentarzy, retweetów cytowań i polubień"
 from tweet t
 where t.type = 1
-order by sum desc
+order by "Suma komentarzy, retweetów cytowań i polubień" desc
 limit 10;
