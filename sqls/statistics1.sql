1837023998656d469d9038bebdffc7606d0a43ba
jakubowiczish <jakub77plotnikowski@gmail.com>
2021-05-13 21:11:05 +0200

    Add statistics1.ipynb

--- sqls/statistics1.sql
+++ sqls/statistics1.sql
@@ -1,5 +1,5 @@
 -- userzy którzy pisali same pozytywne tweety
-select u.name
+select u.name as "Użytkownicy piszący same pozytywne tweety"
 from "user" u
 where u.id not in (select t.user_id
                    from tweet t
@@ -7,7 +7,7 @@ where u.id not in (select t.user_id
                      and t.sentiment_compound < 0.05);
 
 -- userzy którzy pisali same pozytywne komentarze
-select u.name
+select u.name as "Użytkownicy piszący same pozytywne komentarze"
 from "user" u
 where u.id not in (select t.user_id
                    from tweet t
@@ -15,7 +15,7 @@ where u.id not in (select t.user_id
                      and t.sentiment_compound < 0.05);
 
 -- userzy którzy pisali same negatywne tweety
-select u.name
+select u.name as "Użytkownicy piszący same negatywne tweety"
 from "user" u
 where u.id not in (select t.user_id
                    from tweet t
@@ -23,7 +23,7 @@ where u.id not in (select t.user_id
                      and t.sentiment_compound > 0.05);
 
 -- userzy którzy pisali same negatywne komentarze
-select u.name
+select u.name as "Użytkownicy piszący same negatywne komentarze"
 from "user" u
 where u.id not in (select t.user_id
                    from tweet t
@@ -31,138 +31,138 @@ where u.id not in (select t.user_id
                      and t.sentiment_compound > 0.05);
 
 -- ilosc wszystkich tweetów
-select count(*)
+select count(*) as "Ilość wszystkich tweetów"
 from tweet t
 where t.type = 1;
 
 -- ilosc wszystkich komentarzy
-select count(*)
+select count(*) as "Ilość wszystkich komentarzy"
 from tweet t
 where t.type = 2;
 
 -- ilosc wszystkich cytowań
-select count(*)
+select count(*) as "Ilość wszystkich cytowań"
 from tweet t
 where t.type = 3;
 
 -- ilosc wszystkich retweetów
-select count(*)
+select count(*) as "Ilość wszystkich retweetów"
 from retweet;
 
 -- ilosc wszystkich polubień
-select sum(t.favorite_count)
+select sum(t.favorite_count) as "Ilość wszystkich polubień"
 from tweet t;
 
 -- srednia ilosc komentarzy per tweet
-select avg(t.reply_count)
+select avg(t.reply_count) as "Średnia ilość komentarzy na tweet"
 from tweet t
 where t.type = 1;
 
 -- srednia ilosc retweetow per tweet
-select avg(t.retweet_count)
+select avg(t.retweet_count) "Średnia ilość retweetów na tweet"
 from tweet t
 where t.type = 1;
 
 -- srednia ilosc polubien per tweet
-select avg(t.favorite_count)
+select avg(t.favorite_count) "Średnia ilość polubień na tweet"
 from tweet t
 where t.type = 1;
 
 -- srednia ilosc cytowan per tweet
-select avg(t.quote_count)
+select avg(t.quote_count) "Średnia ilość cytowań na tweet"
 from tweet t
 where t.type = 1;
 
 -- srednia ilosc komentarzy per tweet na dzien
-select avg(t.reply_count) as "Srednia liczba komentarzy na dzien", date_trunc('day', t.created_at) as day
+select avg(t.reply_count) as "Średnia liczba komentarzy na dzień", date_trunc('day', t.created_at) as "Dzień"
 from tweet t
 where t.type = 1
-group by day
-order by day desc;
+group by "Dzień"
+order by "Dzień" desc;
 
 -- srednia ilosc komentarzy per tweet na tydzien
-select avg(t.reply_count) as "Srednia liczba komentarzy na tydzien", date_trunc('week', t.created_at) as week
+select avg(t.reply_count) as "Średnia liczba komentarzy na tydzień", date_trunc('week', t.created_at) as "Tydzień"
 from tweet t
 where t.type = 1
-group by week
-order by week desc;
+group by "Tydzień"
+order by "Tydzień" desc;
 
 -- srednia ilosc komentarzy per tweet na miesiac
-select avg(t.reply_count) as "Srednia liczba komentarzy na miesiac", date_trunc('month', t.created_at) as month
+select avg(t.reply_count) as "Średnia liczba komentarzy na miesiąc", date_trunc('month', t.created_at) as "Miesiąc"
 from tweet t
 where t.type = 1
-group by month
-order by month desc;
+group by "Miesiąc"
+order by "Miesiąc" desc;
 
 
 -- srednia ilosc retweetow per tweet na dzien
-select avg(t.retweet_count) as "Srednia liczba retweetow na dzien", date_trunc('day', t.created_at) as day
+select avg(t.retweet_count) as "Średnia liczba retweetow na dzien", date_trunc('day', t.created_at) as "Dzień"
 from tweet t
 where t.type = 1
-group by day
-order by day desc;
+group by "Dzień"
+order by "Dzień" desc;
 
 -- srednia ilosc retweetow per tweet na tydzien
-select avg(t.retweet_count) as "Srednia liczba retweetow na tydzien", date_trunc('week', t.created_at) as week
+select avg(t.retweet_count) as "Srednia liczba retweetow na tydzien", date_trunc('week', t.created_at) as "Tydzień"
 from tweet t
 where t.type = 1
-group by week
-order by week desc;
+group by "Tydzień"
+order by "Tydzień" desc;
 
 -- srednia ilosc retweetow per tweet na miesiac
-select avg(t.retweet_count) as "Srednia liczba retweetow na miesiac", date_trunc('month', t.created_at) as month
+select avg(t.retweet_count) as "Srednia liczba retweetow na miesiac", date_trunc('month', t.created_at) as "Miesiąc"
 from tweet t
 where t.type = 1
-group by month
-order by month desc;
+group by "Miesiąc"
+order by "Miesiąc" desc;
 
 
 -- srednia ilosc polubien per tweet na dzien
-select avg(t.favorite_count) as "Srednia liczba polubien na dzien", date_trunc('day', t.created_at) as day
+select avg(t.favorite_count) as "Srednia liczba polubien na dzien", date_trunc('day', t.created_at) as "Dzień"
 from tweet t
 where t.type = 1
-group by day
-order by day desc;
+group by "Dzień"
+order by "Dzień" desc;
 
 -- srednia ilosc polubien per tweet na tydzien
-select avg(t.favorite_count) as "Srednia liczba polubien na tydzien", date_trunc('week', t.created_at) as week
+select avg(t.favorite_count) as "Srednia liczba polubien na tydzien", date_trunc('week', t.created_at) as "Tydzień"
 from tweet t
 where t.type = 1
-group by week
-order by week desc;
+group by "Tydzień"
+order by "Tydzień" desc;
 
 -- srednia ilosc polubien per tweet na miesiac
-select avg(t.favorite_count) as "Srednia liczba polubien na miesiac", date_trunc('month', t.created_at) as month
+select avg(t.favorite_count) as "Srednia liczba polubien na miesiac", date_trunc('month', t.created_at) as "Miesiąc"
 from tweet t
 where t.type = 1
-group by month
-order by month desc;
+group by "Miesiąc"
+order by "Miesiąc" desc;
 
 
 -- srednia ilosc cytowan per tweet na dzien
-select avg(t.quote_count) as "Srednia liczba cytowan na dzien", date_trunc('day', t.created_at) as day
+select avg(t.quote_count) as "Srednia liczba cytowan na dzien", date_trunc('day', t.created_at) as "Dzień"
 from tweet t
 where t.type = 1
-group by day
-order by day desc;
+group by "Dzień"
+order by "Dzień" desc;
 
 -- srednia ilosc cytowan per tweet na tydzien
-select avg(t.quote_count) as "Srednia liczba cytowan na tydzien", date_trunc('week', t.created_at) as week
+select avg(t.quote_count) as "Srednia liczba cytowan na tydzien", date_trunc('week', t.created_at) as "Tydzień"
 from tweet t
 where t.type = 1
-group by week
-order by week desc;
+group by "Tydzień"
+order by "Tydzień" desc;
 
 -- srednia ilosc cytowan per tweet na miesiac
-select avg(t.quote_count) as "Srednia liczba cytowan na miesiac", date_trunc('month', t.created_at) as month
+select avg(t.quote_count) as "Srednia liczba cytowan na miesiac", date_trunc('month', t.created_at) as "Miesiąc"
 from tweet t
 where t.type = 1
-group by month
-order by month desc;
+group by "Miesiąc"
+order by "Miesiąc" desc;
 
 
 -- srednia ilosc pozytywnych komentarzy per tweet
-select avg(count)
+select avg(count) as "Średnia liczba pozytywnych komentarzy na tweet"
 from (select count(*) as count
       from tweet t
                join tweet t2 on t.id = t2.reply_to
@@ -173,7 +173,7 @@ from (select count(*) as count
       order by count desc) as counts;
 
 -- srednia ilosc negatywnych komentarzy per tweet
-select avg(count)
+select avg(count) "Średnia liczba negatywnych komentarzy na tweet"
 from (select count(*) as count
       from tweet t
                join tweet t2 on t.id = t2.reply_to
@@ -185,7 +185,7 @@ from (select count(*) as count
 
 
 -- srednia ilosc pozytywnych komentarzy per tweet per dzien
-select avg(count), day
+select avg(count) as "Średnia liczba pozytywnych komentarzy na tweet", day as "Dzień"
 from (select count(*) as count, date_trunc('day', t.created_at) as day
       from tweet t
                join tweet t2 on t.id = t2.reply_to
@@ -197,7 +197,7 @@ from (select count(*) as count, date_trunc('day', t.created_at) as day
 group by day;
 
 -- srednia ilosc pozytywnych komentarzy per tweet per tydzien
-select avg(count), week
+select avg(count) as "Średnia liczba pozytywnych komentarzy na tweet", week as "Tydzień"
 from (select count(*) as count, date_trunc('week', t.created_at) as week
       from tweet t
                join tweet t2 on t.id = t2.reply_to
@@ -209,7 +209,7 @@ from (select count(*) as count, date_trunc('week', t.created_at) as week
 group by week;
 
 -- srednia ilosc pozytywnych komentarzy per tweet per miesiac
-select avg(count), month
+select avg(count) as "Średnia liczba pozytywnych komentarzy na tweet", month as "Miesiąc"
 from (select count(*) as count, date_trunc('month', t.created_at) as month
       from tweet t
                join tweet t2 on t.id = t2.reply_to
@@ -222,7 +222,7 @@ group by month;
 
 
 -- srednia ilosc negatywnych komentarzy per tweet per dzien
-select avg(count), day
+select avg(count) as "Średnia liczba negatywnych komentarzy na tweet", day as "Dzień"
 from (select count(*) as count, date_trunc('day', t.created_at) as day
       from tweet t
                join tweet t2 on t.id = t2.reply_to
@@ -234,7 +234,7 @@ from (select count(*) as count, date_trunc('day', t.created_at) as day
 group by day;
 
 -- srednia ilosc negatywnych komentarzy per tweet per tydzien
-select avg(count), week
+select avg(count) as "Średnia liczba negatywnych komentarzy na tweet", week as "Tydzień"
 from (select count(*) as count, date_trunc('week', t.created_at) as week
       from tweet t
                join tweet t2 on t.id = t2.reply_to
@@ -246,7 +246,7 @@ from (select count(*) as count, date_trunc('week', t.created_at) as week
 group by week;
 
 -- srednia ilosc negatywnych komentarzy per tweet per miesiac
-select avg(count), month
+select avg(count) as "Średnia liczba negatywnych komentarzy na tweet", month as "Miesiąc"
 from (select count(*) as count, date_trunc('month', t.created_at) as month
       from tweet t
                join tweet t2 on t.id = t2.reply_to
